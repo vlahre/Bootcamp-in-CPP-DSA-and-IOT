3b2804edf40e1614fb4ed63e3d9b2f5f3c5f0dbb
vlahre <vlahre2000@gmail.com>
2022-10-16 17:05:49 +0530

    assignment-22 complete

---
+++ assignment-22/4_print_text_using_dynamic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    char *text;
+    
+    printf("Enter limit of the text: ");
+    scanf("%d",&n);    
+  
+    text=(char*)malloc(n*sizeof(char));    
+    printf("Enter text: ");
+    scanf(" "); 
+    gets(text);
+    
+    printf("Inputted text is: %s\n",text);   
+  
+    free(text);
+    
+    return 0;
+}
